c8e95da64e486dc0918af8496dae37de10b9d65b
fariaesha-prog <fariaesha2003@gmail.com>
2026-04-17 15:23:13 +0600

    initial set up done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import Home from "./pages/Home";
+import FriendDetails from "./pages/FriendsDetails";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/friend/:id", element: <FriendDetails /> },
+      { path: "/timeline", element: <Timeline /> },
+      { path: "/stats", element: <Stats /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
